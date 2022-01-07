7a87e5a424c6bf59f11be9382c95f3bc5d5e931f
djblackett <49618491+djblackett@users.noreply.github.com>
2022-01-07 18:08:15 -0400

    change price types to number and rename Price.scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
 import Navbar from "./Components/Navbar";
+import Price from "./Components/Price";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Price listPrice={250} percentOff={50}/>
     </div>
   );
 }
--- src/Components/Price.jsx
+++ src/Components/Price.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PropTypes } from "prop-types";
-import "../sass/components/_price.scss";
+import "../sass/components/Price.scss";
 
 export default function Price(props) {
   // Current price is calculated from the list price and percentage off. We can hardcode the sale price if necessary
@@ -10,22 +10,25 @@ export default function Price(props) {
       <div id="current-percent-container">
         <p id="current-price">
           $
-          {(
-            props.listPrice -
-            (Number(props.listPrice) * Number(props.percentOff)) / 100
-          ).toFixed(2)}
+          {
+            // Subtract the percentage off to get the sale price
+            (
+              props.listPrice -
+              (props.listPrice * props.percentOff) / 100
+            ).toFixed(2)
+          }
         </p>
         <p id="percent-off">${props.percentOff}%</p>
       </div>
       <p id="list-price">
-        <span className="strike"></span>${String(props.listPrice)}
+        {/* The list price is intentionally outside the span for the custim strikethrough */}
+        <span className="strike"></span>${String(props.listPrice.toFixed(2))}
       </p>
     </div>
   );
 }
 
 Price.propTypes = {
-  listPrice: PropTypes.string,
-  percentOff: PropTypes.string,
-  width: PropTypes.string,
+  listPrice: PropTypes.number,
+  percentOff: PropTypes.number,
 };

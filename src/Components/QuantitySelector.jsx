99b341a0232c1adc32cd157e7e62b018ea9729d5
Goku-kun <jethvadharmarajsinh@gmail.com>
2022-01-11 18:39:07 +0530

    UPDATE: file formatting

--- src/Components/QuantitySelector.jsx
+++ src/Components/QuantitySelector.jsx
@@ -1,25 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./../sass/components/quantity_selector.scss";
 
-export default function QuantitySelector () {
-
+export default function QuantitySelector() {
   const [quantity, setQuantity] = useState(0);
 
   // define event handlers
-  const reduce = () =>
-    setQuantity((prevQuantity) => prevQuantity - 1);
-  const increment = () =>
-    setQuantity((prevQuantity) => prevQuantity + 1);
+  const reduce = () => setQuantity((prevQuantity) => prevQuantity - 1);
+  const increment = () => setQuantity((prevQuantity) => prevQuantity + 1);
 
   // determine if max or min quantity
   const minQuantity = quantity === 0;
   const maxQuantity = quantity === 5;
 
-  return(
+  return (
     <div className="quantity-selector">
-      <button type="button" disabled={minQuantity} onClick={reduce}>-</button>
+      <button type="button" disabled={minQuantity} onClick={reduce}>
+        -
+      </button>
       <div>{quantity}</div>
-      <button type="button" disabled={maxQuantity} onClick={increment}>+</button>
+      <button type="button" disabled={maxQuantity} onClick={increment}>
+        +
+      </button>
     </div>
   );
 }

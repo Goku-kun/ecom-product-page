6c7e4524e2d2e646882d403b70bfc6df1b9302a0
Goku-kun <jethvadharmarajsinh@gmail.com>
2022-01-12 20:33:04 +0530

    UPDATE: functional nav bar and curtain menu with mobile first design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.scss";
 import Navbar from "./Components/Navbar";
-import Price from "./Components/Price";
+// import Price from "./Components/Price";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Price listPrice={250} percentOff={50} />
+      {/* <Price listPrice={250} percentOff={50} /> */}
     </div>
   );
 }

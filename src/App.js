7ea8f7877932569530a1865b840659ba9550d6ec
djblackett <49618491+djblackett@users.noreply.github.com>
2022-01-12 20:45:10 -0400

    add Slider component and styling.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import "./App.scss";
 import Navbar from "./Components/Navbar";
 import Price from "./Components/Price";
+import Slider from "./Components/Slider";
+import { useSelector } from "react-redux";
+import { selectDefaultProduct } from "./features/products/productsSlice";
 
 function App() {
+  const imagesArray = useSelector(selectDefaultProduct).displayPictures;
+
   return (
     <div className="App">
       <Navbar />
-      <Price listPrice={250} percentOff={50}/>
+      <Price listPrice={250} percentOff={50} />
+      <div id="slider-container">
+        <Slider imagesArray={imagesArray} />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Slider.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { PropTypes } from "prop-types";
+import "../sass/components/Slider.scss";
+
+export default function Slider(props) {
+  
+
+  // The array of images will either come from props or redux
+  // I tried a few approaches to managing the state and have more 
+  // questions than answers at this point
+
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handlePreviousImage = () => {
+    let length = props.imagesArray.length;
+    if (currentImageIndex > 0) {
+      setCurrentImageIndex(currentImageIndex - 1);
+    } else {
+      setCurrentImageIndex(length - 1);
+    }
+  };
+
+  const handleNextImage = () => {
+    let length = props.imagesArray.length;
+    if (currentImageIndex < length - 1) {
+      setCurrentImageIndex(currentImageIndex + 1);
+    } else {
+      setCurrentImageIndex(0);
+    }
+  };
+
+  const url = {
+    backgroundImage: `url(${props.imagesArray[currentImageIndex]})`,
+  };
+
+  return (
+    <div id="slider" style={url}>
+      <div className="white-circle" onClick={handlePreviousImage}>
+        <img
+          id="left-arrow"
+          className="arrow-icon"
+          src="./images/icon-previous.svg"
+        />
+      </div>
+      <div className="white-circle" onClick={handleNextImage}>
+        <img
+          id="right-arrow"
+          className="arrow-icon"
+          src="./images/icon-next.svg"
+        />
+      </div>
+    </div>
+  );
+}
+
+Slider.propTypes = {
+  imagesArray: PropTypes.array.isRequired
+};

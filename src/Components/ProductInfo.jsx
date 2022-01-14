c5817dcdd5b5054eab4b19153322aa68039aa5b1
djblackett <49618491+djblackett@users.noreply.github.com>
2022-01-14 14:39:48 -0400

    add productInfo component and styling

---
+++ src/Components/ProductInfo.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./../sass/components/ProductInfo.scss";
+
+function productInfo() {
+  const productInfo = {
+    companyName: "Sneaker Company",
+    productName: "Fall Limited Edition Sneakers",
+    productDescription:
+      "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer",
+  };
+
+  return (
+    <div id="product-info-container">
+      <p id="company-name">{productInfo.companyName.toUpperCase()}</p>
+
+      <h1 id="product-name">{productInfo.productName}</h1>
+      <p id="product-description">{productInfo.productDescription}</p>
+    </div>
+  );
+}
+
+export default productInfo;

6e657577782953a57ef2984ecddf1f991dcc7b70
djblackett <49618491+djblackett@users.noreply.github.com>
2022-01-15 20:57:08 -0400

    Delete scratch file.js
    I missed this file when I was removing files I mistakenly committed.

--- src/features/products/scratch file.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-const items = [
-  { name: "bob", category: "person" },
-  { name: "fred", category: "person" },
-  { name: "jack", category: "rabbit" },
-];
-const allCategories = ["all", ...new Set(items.map((item) => item.category))];
-console.log(allCategories);
